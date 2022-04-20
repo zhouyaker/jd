6ff60f60493e68f8c42d60b70790344cd2f9c498
yabo0106 <yabo0106@163.com>
2022-04-20 18:37:51 +0800

    初始化

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,51 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <span class="app_item active">
+      <span class="app_item_iconfont iconfont">&#xe600;</span>
+      <span class="app_item_text">首页</span>
+    </span>
+    <span class="app_item">
+      <span class="app_item_iconfont iconfont">&#xe70b;</span>
+      <span class="app_item_text">购物车</span>
+    </span>
+    <span class="app_item">
+      <span class="app_item__iconfont iconfont">&#xe645;</span>
+      <span class="app_item_text">订单</span>
+    </span>
+    <span class="app_item">
+      <span class="app_item_iconfont iconfont">&#xe601;</span>
+      <span class="app_item_text">我的</span>
+    </span>
+  </div>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+  width: 375rem;
+  height: 49rem;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: #fff;
+  box-shadow: 0 -1rem 1rem 0 #f1f1f1;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  .app_item {
+    display: flex;
+    // color: #333;
+    flex-direction: column;
+    align-items: center;
+    .iconfont {
+      font-size: 25rem;
     }
+    .text {
+      font-size: 10rem;
+    }
+  }
+  .active {
+    color: #008efb;
   }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,4 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 初始化样式
+import 'normalize.css'
+import './style/base.scss'
+
 createApp(App).use(store).use(router).mount('#app')
